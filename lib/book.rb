dac9bb0d5c77172001cd4ec9dc13d3bd57a21c98
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-24 09:22:15 -0700

    Feat: add book class and methods.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,19 @@
 class Book
-  
+  attr_reader :title
+
+  def initialize(book_info)
+    @author_first_name = book_info[:author_first_name]
+    @author_last_name = book_info[:author_last_name]
+    @title = book_info[:title]
+    @publication_date = book_info[:publication_date]
+  end
+
+  def author
+    "#{@author_first_name} #{@author_last_name}"
+  end
+
+  def publication_year
+    date_array = @publication_date.split(" ")
+    date_array[2]
+  end
 end
